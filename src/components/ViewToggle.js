f17a3b709f2e715a29c40755e9d4a7ca3c8fcac6
divyasubramanian291 <divyasubramanian291@gmail.com>
2025-09-29 08:58:16 +0530

    Table and Card view buttons color change based on selection

--- src/components/ViewToggle.js
+++ src/components/ViewToggle.js
@@ -3,13 +3,37 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import ListIcon from '@mui/icons-material/List';
 import GridOnIcon from '@mui/icons-material/GridOn';
+
 export default function ViewToggle({ view, setView }) {
   return (
     <Stack direction="row" spacing={2} paddingBottom={2}>
-      <Button variant="outlined" onClick={()=>setView("table")} startIcon={<GridOnIcon />}>
+      <Button
+        variant="outlined"
+        onClick={() => setView("table")}
+        startIcon={<GridOnIcon />}
+        sx={{
+          color: view === "table" ? "primary.main" : "gray",
+          borderColor: view === "table" ? "primary.main" : "gray",
+          '&:hover': {
+            borderColor: view === "table" ? "primary.dark" : "gray",
+          },
+        }}
+      >
         Table
       </Button>
-      <Button variant="outlined" color="gray" onClick={()=>setView("card")} startIcon={<ListIcon />}>
+
+      <Button
+        variant="outlined"
+        onClick={() => setView("card")}
+        startIcon={<ListIcon />}
+        sx={{
+          color: view === "card" ? "primary.main" : "gray",
+          borderColor: view === "card" ? "primary.main" : "gray",
+          '&:hover': {
+            borderColor: view === "card" ? "primary.dark" : "gray",
+          },
+        }}
+      >
         Card
       </Button>
     </Stack>
